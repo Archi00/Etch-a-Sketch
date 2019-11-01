768b100e415c773bbe816e551d1f3e545c15c91b
Archi00 <albert.archi@gmail.com>
2019-11-01 17:14:58 +0100

    Created and styled the grid, is functional

---
+++ script.js
@@ -0,0 +1,25 @@
+const container = document.getElementById("container");
+rows = document.getElementsByClassName("gridRow");
+columns = document.getElementsByClassName("gridColumn");
+
+myGrid();
+
+function myGrid(){
+    makeRows(16);
+    makeColumns(16);
+};
+function makeRows(rowNumber){
+    for (let j = 0; j < rowNumber; j++){
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
+};
+function makeColumns(columnNumber){
+    for (let i = 0; i < rows.length; i++){
+        for (let x = 0; x < columnNumber; x++){
+            let column = document.createElement("div");
+            rows[x].appendChild(column).className = "gridColumn";
+        };
+    };
+};
+
